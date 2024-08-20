180aaed3801b140b0aa3c280d1c3ec0c4aa850f6
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-08-20 14:29:16 +0300

    Initial Commit

---
+++ App760/App760App.swift
@@ -0,0 +1,17 @@
+//
+//  App760App.swift
+//  App760
+//
+//  Created by IGOR on 20/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct App760App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
